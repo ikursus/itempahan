558769dd6919768b7afdb2902a0fd473f8c45d0f
Amirol Zolkifli <sitehandy@gmail.com>
2024-10-28 12:02:03 +0800

    update borang

--- resources/views/email/template-borang.blade.php
+++ resources/views/email/template-borang.blade.php
@@ -20,19 +20,34 @@
 
     <div class="col-12">
 
-      <div class="card">
-        <div class="card-header">
-          <h3 class="card-title">Invoices</h3>
+        <div class="card">
+            <div class="card-header">
+                <h3 class="card-title">Kandungan Email</h3>
+            </div>
+
+
+            <form action="{{ route('hantar.email') }}" method="POST">
+                @csrf
+                <div class="card-body border-bottom py-3">
+
+                    <div class="mb-3 row">
+                        <label class="required">Tajuk Email</label>
+                        <input type="text" class="form-control" name="tajuk_email" required>
+                    </div>
+
+                    <div class="mb-3 row">
+                        <label class="required">Kandungan Email</label>
+                        <textarea class="form-control" name="kandungan_email" required></textarea>
+                    </div>
+
+                </div>
+
+                <div class="card-footer d-flex align-items-center">
+                    <button type="submit" class="btn btn-primary">Hantar</button>
+                    <a href="{{ route('dashboard.pelanggan') }}" class="btn btn-link">Kembali</a>
+                </div>
+            </form>
         </div>
-        <div class="card-body border-bottom py-3">
-
-        </div>
-
-        <div class="card-footer d-flex align-items-center">
-
-        </div>
-      </div>
-
 
     </div>
   </div>
