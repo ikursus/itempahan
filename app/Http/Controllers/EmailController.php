a386976a3172a8f94a9a9ba9ca63e795c200c79b
Amirol Zolkifli <sitehandy@gmail.com>
2024-10-28 12:36:31 +0800

    Update email

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use App\Mail\EmailUmum;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class EmailController extends Controller
 {
@@ -13,6 +16,26 @@ class EmailController extends Controller
 
     public function hantarEmail(Request $request)
     {
+        // Validasi data borang
+        $request->validate([
+            'tajuk_email' => 'required|min:3',
+            'kandungan_email' => ['required', 'min:3'],
+        ]);
 
+        // Dapatkan data yang diperlukan oleh EmailUmum
+        $tajukEmail = $request->input('tajuk_email');
+        $kandunganEmail = $request->input('kandungan_email');
+
+        // Dapatkan senarai users yang akan menerima emails
+        $users = User::all();
+
+        // Menghantar email ke semua users yang dipilih
+        foreach ($users as $user)
+        {
+            Mail::to($user->email)->send(new EmailUmum($tajukEmail, $kandunganEmail));
+        }
+
+        // Return status
+        return 'Email sukses dihantar';
     }
 }
---
+++ app/Mail/EmailUmum.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class EmailUmum extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $tajukEmail;
+    public $kandunganEmail;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct($tajukEmail, $kandunganEmail)
+    {
+        $this->tajukEmail = $tajukEmail;
+        $this->kandunganEmail = $kandunganEmail;
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: $this->tajukEmail,
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            view: 'email.template-kandungan-email',
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
